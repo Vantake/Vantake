7d28af2bbb8ac1e6dc666a57304aac7dc92be798
Vantake <letuzinukure67@gmail.com>
2026-02-21 23:36:26 +0700

    Trader analytics profile with performance breakdown

--- app/trader/[id]/page.tsx
+++ app/trader/[id]/page.tsx
@@ -10,7 +10,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 import {
   type UserPosition,
   type UserTrade,
-  type LeaderboardTrader,
+  type LeaderboardTrader, 
   formatPnl,
   formatVolume,
   formatAddress,
