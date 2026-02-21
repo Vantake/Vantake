79ac302e98a75e4855f6201069af9f535fcb98e7
Vantake <letuzinukure67@gmail.com>
2026-02-21 23:46:35 +0700

    Application-level request middleware for routing and access control

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
-}
+} 
 
 export const config = {
   matcher: [
